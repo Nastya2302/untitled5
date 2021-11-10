0127bb568fd3cc36e19a99557800ef8058667b73
Acer <nastyakharchenko23@gmail.com>
2021-11-10 10:17:35 +0200

    LR5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,19 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        double y;
-        System.out.print("Результати циклу: ");
-        for (double x=-1;x<=1;x+=0.1) {
-            if(x==0)
-                System.out.println("Ділити на 0 не можна!");
-            y = Math.pow(x, (1/x));
-            System.out.print(y+"\n");
+        int Sum=0, Avg=0;
+        Scanner scanner = new Scanner(System.in);
+        int[] ary = new int[3];
+        for (int i = 0; i < 3; i++)
+        {
+            System.out.print("Число= ");
+            ary[i]=scanner.nextInt();
+            Sum+=ary[i];
         }
+        Avg=Sum/3;
+        System.out.println("Cереднє значення ="+Avg);
+    }
+
 
     }
-}
+
